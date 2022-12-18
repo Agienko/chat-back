6d16544b6680ba05fc7d48f9379c87789f088243
Yuriy Agienko <yuriy.agienko@getta.io>
2022-12-18 16:56:39 +0200

    change get to query

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require('express')
 const cors = require('cors')
+const {add} = require("nodemon/lib/rules");
 
 const app = express()
 
@@ -24,64 +25,88 @@ app.listen(PORT, error=>{
 })
 
 app.get('/', (req, res)=>{
-    res.json(data)
-})
+    const addMessage = req?.query?.addMessage
+    const addUser = req?.query?.addUser
+    const delUser = req?.query?.delUser
 
 
+    if(addMessage) {
+        const user = req.query?.user
 
-app.route('/users').get((req, res)=>{
-    res.json(data.users)
-})
-
-app.get('/addUser', (req, res)=>{
-    const user = req.query?.user
-   if(user){
-       if(data.users.includes(user)){
-           res.json({"userAdded": false, "status": "user already exists"})
-       } else {
-           data.users.push(user)
-           res.json({"userAdded": true, "status": "success"})
-       }
-   } else {
-       res.status(404)
-           .json({"userAdded": false, "status": "error"})
-   }
-
-})
-
-app.get('/delUser', (req, res)=>{
-    const user = req.query?.user
-    if(user){
-        if(!data.users.includes(user)){
-            res.json({"delete": false, "status": "user is not exist"})
-        } else {
-            data.users = data.users.filter(el=> el !== user)
-            res.json({"delete": true, "status": "success"})
-        }
-    } else {
-        res.status(404)
-            .json({"delete": false, "status": "error"})
-    }
-
-})
-
-app.get('/messages', (req, res)=>{
-    res.json(data.messages)
-})
-
-app.get('/addMessage', (req, res)=>{
-    const message = req.query?.message
-    const user = req.query?.user
-    if(message){
-            const messageObj = {"user": user, "message": message, "date": new Date}
+        const messageObj = {"user": user, "message": addMessage, "date": new Date}
         data.messages.push(messageObj)
         res.json({"messageAdded": true, "status": "success"})
 
-    } else {
-        res.status(404).json({"messageAdded": false, "status": "error"})
+    }
+    else if(addUser){
+        if(data.users.includes(addUser)){
+           res.json({"userAdded": false, "status": "user already exists"})
+       } else {
+           data.users.push(addUser)
+           res.json({"userAdded": true, "status": "success"})
+       }
+    }
+    else if (delUser){
+        if(!data.users.includes(delUser)){
+            res.json({"delete": false, "status": "user is not exist"})
+        } else {
+            data.users = data.users.filter(el=> el !== delUser)
+            res.json({"delete": true, "status": "success"})
+        }
+    }
+    else {
+        res.json(data)
     }
 
+
 })
+
+// app.get('/addUser', (req, res)=>{
+//     const user = req.query?.user
+//    if(user){
+//        if(data.users.includes(user)){
+//            res.json({"userAdded": false, "status": "user already exists"})
+//        } else {
+//            data.users.push(user)
+//            res.json({"userAdded": true, "status": "success"})
+//        }
+//    } else {
+//        res.status(404)
+//            .json({"userAdded": false, "status": "error"})
+//    }
+//
+// })
+//
+// app.get('/delUser', (req, res)=>{
+//     const user = req.query?.user
+//     if(user){
+//         if(!data.users.includes(user)){
+//             res.json({"delete": false, "status": "user is not exist"})
+//         } else {
+//             data.users = data.users.filter(el=> el !== user)
+//             res.json({"delete": true, "status": "success"})
+//         }
+//     } else {
+//         res.status(404)
+//             .json({"delete": false, "status": "error"})
+//     }
+//
+// })
+//
+//
+// app.get('/addMessage', (req, res)=>{
+//     const message = req.query?.message
+//     const user = req.query?.user
+//     if(message){
+//             const messageObj = {"user": user, "message": message, "date": new Date}
+//         data.messages.push(messageObj)
+//         res.json({"messageAdded": true, "status": "success"})
+//
+//     } else {
+//         res.status(404).json({"messageAdded": false, "status": "error"})
+//     }
+//
+// })
 
 
 app.use((req, res)=>{
